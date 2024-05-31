7132c545c96cad138639ed79882979bd9909a9ac
Ryristy <ristyfebriani8@gmail.com>
2024-05-31 16:55:50 +0700

    praktikum 2 new

---
+++ praktikum 3 new/proses_registrasi.php
@@ -0,0 +1,15 @@
+<?php
+// Array Numerik - Indek & Value
+$domisili = ["Jakarta", "Bogor", "Depok", "Tanggerang", "Bekasi"];
+
+// Array Assosiative - Key & Value
+$program_studi = [
+"SI" => "Sistem Informasi",
+"TI" => "Teknik Informatika",
+"BD" => "Bisnis Digital",
+
+];
+
+
+
+?>
